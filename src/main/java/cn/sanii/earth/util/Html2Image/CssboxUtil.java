b2bce31aab79c8b97f5370365aa47d9809e97cb5
王首梁 <shouliang.wang@qunar.com>
2019-03-20 13:34:43 +0800

    cssbox封装

---
+++ src/main/java/cn/sanii/earth/util/Html2Image/CssboxUtil.java
@@ -0,0 +1,81 @@
+package cn.sanii.earth.util.Html2Image;
+
+import org.fit.cssbox.demo.ImageRenderer;
+import org.xml.sax.SAXException;
+
+import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author: shouliang.wang
+ * @create: 2019-03-20 13:17
+ * @description: Cssbox html生成图片工具类
+ **/
+public class CssboxUtil {
+
+    public static final int WIDTH = 1900;
+    public static final int HEIGHT = 1000;
+
+    /**
+     * 图片生成到指定路径
+     * 支持 file ftp http https
+     *
+     * @param url  html地址
+     * @param path 生成文件地址
+     * @throws IOException
+     * @throws SAXException
+     */
+    public static void toPath(String url, String path) throws IOException, SAXException {
+        toPath(url, path, WIDTH, HEIGHT);
+    }
+
+    /**
+     * 图片生成到指定路径
+     * 支持 file ftp http https
+     *
+     * @param url  html地址
+     * @param path 生成文件地址
+     * @throws IOException
+     * @throws SAXException
+     */
+    public static void toPath(String url, String path, int width, int height) throws IOException, SAXException {
+        ImageRenderer render = new ImageRenderer();
+        FileOutputStream out = new FileOutputStream(new File(path));
+        render.setWindowSize(new Dimension(width, height), false);
+        render.renderURL(url, out, ImageRenderer.Type.PNG);
+        out.close();
+    }
+
+    /**
+     * 图片生成到byte数组
+     * 支持 file ftp http https
+     *
+     * @param url html地址
+     * @throws IOException
+     * @throws SAXException
+     */
+    public static byte[] toByte(String url) throws IOException, SAXException {
+        return toByte(url, WIDTH, HEIGHT);
+    }
+
+    /**
+     * 图片生成到byte数组
+     * 支持 file ftp http https
+     *
+     * @param url html地址
+     * @throws IOException
+     * @throws SAXException
+     */
+    public static byte[] toByte(String url, int width, int height) throws IOException, SAXException {
+        ImageRenderer render = new ImageRenderer();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        render.setWindowSize(new Dimension(1900, 1000), false);
+        render.renderURL(url, stream, ImageRenderer.Type.PNG);
+        byte[] bytes = stream.toByteArray();
+        stream.close();
+        return bytes;
+    }
+}
--- src/main/test/Html2ImageTest.java
+++ src/main/test/Html2ImageTest.java
@@ -1,14 +1,11 @@
+import cn.sanii.earth.util.Html2Image.CssboxUtil;
 import cn.sanii.earth.util.Html2Image.Html2ImageUtil;
 import cn.sanii.earth.util.Html2Image.PrintScreen4DJNativeSwingUtils;
 import cn.sanii.earth.util.OkHttpUtil;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import org.fit.cssbox.demo.ImageRenderer;
 import org.xml.sax.SAXException;
 
-import java.awt.*;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -22,8 +19,8 @@ public class Html2ImageTest {
     public static final String URL = "http://www.travelsky.com/tsky/validate.jsp";
 
     public static void main(String[] args) throws IOException, SAXException {
-        Html2Image();
-        JWebBrowser();
+//        Html2Image();
+//        JWebBrowser();
         cssbox();
     }
 
@@ -42,14 +39,9 @@ public class Html2ImageTest {
     }
 
     public static void cssbox() throws IOException, SAXException {
-        ImageRenderer render = new ImageRenderer();
-        System.out.println("start...");
         String url = "http://www.travelsky.com/tsky/validate.jsp";
-        FileOutputStream out = new FileOutputStream(new File("D:\\4_" + System.currentTimeMillis() + ".png"));
-        render.setWindowSize(new Dimension(1900, 1000), false);
-        render.renderURL(url, out, ImageRenderer.Type.PNG);
-        out.close();
-        System.out.println("OK");
+        CssboxUtil.toPath(url, "D:\\4_" + System.currentTimeMillis() + ".png");
+        System.out.println(CssboxUtil.toByte(url).length);
     }
 
 
